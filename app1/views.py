6792c3d299d0207c6aa26d1964dccb49a8540a94
MManoj11 <mannammanoj11@gmail.com>
2023-03-15 13:16:22 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def samplex(request):
+    return render(request,'samplex.html')
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sampley(request):
+    return render(request,'sampley.html')
